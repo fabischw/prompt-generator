5ec2d084194df2c4706da029d70942a6b2aefa3d
fabischw <107812436+fabischw@users.noreply.github.com>
2023-02-12 16:38:57 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+This is a streamlit script for a small webapp that creates a ChatGPT coding project prompt.
+"""
+
+
+
+
+languages = ["python","javascript","java","C","lua","ruby","C++","C#","Visual Basic","Pascal"]
+
+
+topics = ["loops","if/else","object oriented programming","input/output(text based)","functions","reading/writing files","language specific magic"]
+
+data_types = ["strings","integers","float","arrays","binary trees","search trees"]
+
+
+
